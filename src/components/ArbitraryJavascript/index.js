3169fbaf2fc9309dc404ae2e6687f2bf66fac16e
Steven McKeown <steven@pragmaflow.com>
2022-03-11 08:38:29 -0500

    stuff

--- src/components/ArbitraryJavascript/index.js
+++ src/components/ArbitraryJavascript/index.js
@@ -1,56 +1,76 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 const ArbitraryJavascript = (props) => {
-	const [executionResults, setExecutionResults] = useState('')
-	const { 
-		trigger, 
-		script, 
-		onNumberResults, 
-		onStringResults, 
-		onError, 
-		editor,
-		styles
-	} = props
-	console.log(props)
-	const execute = () => {
-		try {
-			const results = new Function(script)
-			Promise.resolve(results()).then(result => {
-				if (onNumberResults) { 
-					onNumberResults(JSON.stringify(+result));  }
-					if (onStringResults) { 
-					onStringResults(JSON.stringify(result));  }
-				setExecutionResults(JSON.stringify(result))
-			}).catch(error => {
-				if (onError) { onError(JSON.stringify(error)); }
-				setExecutionResults(JSON.stringify(error))
-			})
-		} catch(functionError) {
-			if (editor) { 
-				if (onError) { onError("There is an error in your function"); }
-				setExecutionResults(JSON.stringify(result))
-			}
-		}
-	}
+  const [executionResults, setExecutionResults] = useState("");
+  const {
+    trigger,
+    script,
+    onNumberResults,
+    onStringResults,
+    onError,
+    editor,
+    styles,
+  } = props;
+  const execute = () => {
+    try {
+      const results = new Function(script);
+      Promise.resolve(results())
+        .then((result) => {
+          if (onNumberResults) {
+            onNumberResults(+result);
+						setExecutionResults(+result);
+          }
+					debugger
+					if (typeof result === "string" || result instanceof String) {
+						if (onStringResults) { onStringResults(result); }
+						setExecutionResults(result);
+					} else {
+						if (onStringResults) { onStringResults(JSON.stringify(result)); }
+						setExecutionResults(JSON.stringify(result));
+					}
+        })
+        .catch((error) => {
+          if (onError) {
+            onError(JSON.stringify(error));
+          }
+          setExecutionResults(JSON.stringify(error));
+        });
+    } catch (functionError) {
+      if (editor) {
+        if (onError) {
+          onError("There is an error in your function");
+        }
+				if (typeof result === "string" || result instanceof String) {
+					setExecutionResults(result);
+				} else {
+					setExecutionResults(JSON.stringify(result));
+				}
+      }
+    }
+  };
 
-	useEffect(() => {
-		if (trigger === 'true') {
-			execute()
-		}
-	}, [trigger, editor])
-	
-	return(
-		<View>
-			<Text style={{
-				color: styles.script?.color,
-				fontFamily: styles.script?.fontFamily,
-				fontSize: styles.script?.fontSize,
-				fontWeight: styles.script?.fontWeight,
-				textAlign: styles.script?.textAlign
-			}}>{executionResults}</Text>
-		</View>
-	)
-}
+  useEffect(() => {
+    if (trigger === "true" && !editor) {
+      execute();
+    }
+  }, [trigger, editor]);
 
-export default ArbitraryJavascript
+  return (
+    <View>
+      <Text
+        style={{
+          color: styles.script?.color,
+          fontFamily: styles.script?.fontFamily,
+          fontSize: styles.script?.fontSize,
+          fontWeight: styles.script?.fontWeight,
+          textAlign: styles.script?.textAlign,
+        }}
+      >
+        {`${executionResults}`}
+      </Text>
+    </View>
+  );
+};
+
+export default ArbitraryJavascript;
